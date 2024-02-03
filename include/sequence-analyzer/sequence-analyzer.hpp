d4dd38cd71409889b152b5f2a35df2cad149b22f
asuka1975 <39215088+asuka1975@users.noreply.github.com>
2024-02-03 22:23:11 +0900

    feat: add analyze method to SequenceAnalyzer

--- include/sequence-analyzer/sequence-analyzer.hpp
+++ include/sequence-analyzer/sequence-analyzer.hpp
@@ -1,10 +1,18 @@
 #ifndef ASUKA1975_SEQUENCE_ANALYZER_HPP
 #define ASUKA1975_SEQUENCE_ANALYZER_HPP
 
-namespace asuka1975 {
-    template <class >
-    class SequenceAnalyzer {
+#include <concepts>
+#include <ranges>
 
+#include <result/result.hpp>
+
+namespace asuka1975 {
+    template <class TItem, class TOutput, class TError>
+    class SequenceAnalyzer {
+    public:
+        template <std::ranges::input_range TAnalyzeeSequence>
+            requires std::same_as<std::ranges::range_value_t<TAnalyzeeSequence>, TItem>
+        Result<TError, TOutput> analyze(const TAnalyzeeSequence& sequence);
     };
 }
 
